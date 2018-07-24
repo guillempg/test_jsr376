fcf6b5929ddc82639db18fef5acccf43bb535c25
Guillem <guillemp@leadmolecular.com>
2018-07-24 16:06:45 +0200

    Adding a JUnit test (to eventually see it working in Java 9 modules)

--- com.guillempg.testJSR376/com.guillempg.app/TestApp.java
+++ com.guillempg.testJSR376/com.guillempg.app/TestApp.java
@@ -2,7 +2,13 @@ package com.guillempg.app;
 
 class TestApp{
 
+	private static final String MESSAGE = "Hello Java modules";
+
 	public static void main(String[] args){
-		System.out.println("Hello Java modules");
+		System.out.println(MESSAGE);
+	}
+
+	public static String getMessage(){
+		return MESSAGE; 
 	}
 }
---
+++ com.guillempg.testJSR376/com.guillempg.app/TestClass.java
@@ -0,0 +1,13 @@
+package com.guillempg.app;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestClass{
+
+    @Test
+    public void testGetPointsInDescendingPPMOrder() {
+	String msg = TestApp.getMessage();
+	assertEquals("Hello Java modules", msg);
+    }
+}
